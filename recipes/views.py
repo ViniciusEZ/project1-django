63231a4c21e42ba9c6b76dbadd2d7be87db05454
ViniciusEZ <ez998@outlook.com>
2022-11-15 05:01:12 -0300

    search and header

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "recipes/home.html", 
-                context={'name': 'Vinicius'}, status=201)
+    return render(request, "recipes/pages/home.html", 
+            context={'name': 'Vinicius'}, status=201)
 
-def contato(request):
-    return render(request, 'global/contact.html')
 
-def sobre(request): return HttpResponse('SOBRE!')
 
