dc9eec5e0b06877d51d64cb61afb7eb4031187e3
ViniciusEZ <ez998@outlook.com>
2022-11-20 20:29:11 -0300

    updates

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -14,7 +15,7 @@ class Category(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=70)
     description = models.CharField(max_length=170)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True, null=True)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=70)
     servings = models.IntegerField()
@@ -24,10 +25,17 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d', blank=True, default='')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     
     
     def __str__(self) -> str:
         return self.title
+    
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = f'{slugify(self.title)}'
+            
+        super().save(*args, **kwargs)
